30fae2f5845317bac8472e5bef555bbc55595722
Daria <startceva.daria@gmail.com>
2020-08-03 16:44:52 +0300

    Creating new message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
     before_action :require_user, except: [:index]
     def index
+        @message = Message.new
         @messages = Message.all
     end
 
