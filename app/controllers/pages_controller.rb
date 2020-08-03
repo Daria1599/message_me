bb20ae71f149c3d0be4c8c0d481b981e1af6b1ba
Daria <startceva.daria@gmail.com>
2020-08-03 19:02:24 +0300

    Displaying 20 messages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,9 @@ class PagesController < ApplicationController
     before_action :require_user, except: [:index]
     def index
         @message = Message.new
-        @messages = Message.all
+        @messages = Message.custom_display # изначально тут было Message.all
     end
+
+    
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true,  
                 length: { maximum: 200 }
+    scope :custom_display,  -> { order(:created_at).last(20) } # показывает последние 20 сообщений на странице (custom_display метод указываем в контроллере)
 end
